007b0c3c5115c378bf1651cb72844afbba83a65c
andrea zhou <anzhou@moravia.com>
2023-06-27 15:04:09 +0800

    add instructions about running RA on a new machine

--- FinishRANastran.bat
+++ FinishRANastran.bat
@@ -5,8 +5,8 @@ echo This folder should also contain the old lpu (named like'old_All_Nastran_InC
 
 set /p folder=folder path: e.g. C:\Users\AnZhou\Downloads\temp\RA\Nastran
 set /p build=Type in the build number, e.g.: 45.0.46
-@REM start /b /wait python prepare_RA_folders.py --path=%folder% --project=Nastran
-@REM echo muilt-language lpu files done copying
+start /b /wait python prepare_RA_folders.py --path=%folder% --project=Nastran
+echo muilt-language lpu files done copying
 @REM echo start removing extra languages from lpu, this may take a while...
 @REM for %%i in (chs, cht, fra, deu, ita,jpn,kor) do pslcmd.exe /openproject:%folder%\%%i\CurrentReleaseLpu\%%i_All_Nastran_InCAD.lpu /runmacro=Remove_langs_from_project.bas & pslcmd.exe /openproject:%folder%\%%i\LastReleaseLpu\%%i_All_Nastran_InCAD.lpu /runmacro=Remove_langs_from_project.bas
 @REM echo all the extra languages have been successfully removed from lpus, and mapping files have been created
